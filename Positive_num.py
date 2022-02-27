a15afd5bd0d3c95a3b5294f7dcab277cbafa59d9
sneha-khandekar <78477217+sneha-khandekar@users.noreply.github.com>
2022-02-28 00:04:33 +0530

    Add files via upload

---
+++ Positive_num.py
@@ -0,0 +1,9 @@
+List1 = [12,-7,5,64,-14]
+for num in List1:
+    if num > 0:
+        print(num, end = ",")
+
+List2 = [12,14,-95,3]
+for num1 in List2:
+    if num1 > 0:
+        print(num1, end = ",")
